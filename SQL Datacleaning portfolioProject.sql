38e3f8ce8b05933725de12fd26c3311360a8e0cc
Thiruvariyamuthu <gurusaran041104@gmail.com>
2024-09-17 08:21:47 +0530

    Update SQL Datacleaning portfolioProject.sql

--- SQL Datacleaning portfolioProject.sql
+++ SQL Datacleaning portfolioProject.sql
@@ -15,7 +15,7 @@ SELECT *
 FROM LAYOFFS;
 
 -- now when we are data cleaning we usually follow a few steps
--- 1. check for duplicates and remove any
+-- 1. check for duplicates and remove
 -- 2. standardize data and fix errors
 -- 3. Look at null values and see what 
 -- 4. remove any columns and rows that are not necessary - few ways
